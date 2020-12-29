54c04084cc53d1a7cdf20658e38d94beca8258a4
joker_li <libo1171309228@163.com>
2020-12-29 15:23:12 +0800

    leetcode-567. 字符串的排列 (困难)

--- dp/tricks/Sliding_Window/Window/leetcode_567.py
+++ dp/tricks/Sliding_Window/Window/leetcode_567.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 """ leetcode-567. 字符串的排列 (困难)
 [题目]：
-        给定两个字符串 `s1` 和 `s2`，写一个函数来判断 `s2` 是否包含 `s1` 的排列。
-        换句话说，第一个字符串的排列之一是第二个字符串的子串。
+    给定两个字符串 `s1` 和 `s2`，写一个函数来判断 `s2` 是否包含 `s1` 的排列。
+    换句话说，第一个字符串的排列之一是第二个字符串的子串。
 
 示例1:
 	输入: s1 = "ab" s2 = "eidbaooo"
@@ -18,27 +18,25 @@
 * 两个字符串的长度都在 `[1, 10,000]` 之间
 """
 
-
+# [思路]：将判断字符排列情况转化为各字符个数和字符串长度问题。
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
+    def checkIncclusion(self, s: str, t: str) -> bool:
         if not s:
-            return None
+            return True
         need = {}    # 需要的字符个数
         window = {}  # 窗口包含的字符个数
         # 初始化各字符出现的个数
-        for i in t:
+        for i in s:
             window[i] = 0
-            need[i] += 1
+            need[i] = s.count(i)
 
         # 设置窗口左右边界
         left, right = 0, 0
-        start = 0   # start 记录最小子串的起始位置
         valid = 0  # 记录要达到要求的各字符个数
-        Len = len(s) + len(t)  # 记录最小窗口长度，用于更新
 
         # 开始滑动窗口
-        while right < len(s):
-            cin = s[right]
+        while right < len(t):
+            cin = t[right]
             right += 1
 
             # 若当前遍历字符是需要匹配的字符，则window中对应位置数+1
@@ -49,13 +47,12 @@ class Solution:
                     valid += 1
 
             # 若当前所有子串的字符都被包含，那么此时窗口开始收缩
-            while valid == len(need):
-                # 更新窗口大小
-                if right - left <= Len:
-                    Len = right - left
-                    start = left    # 并更新最小子串的起始位置
+            while (right - left > len(need)):
+                # 判断是否找到了合法子串
+                if valid == len(need):
+                    return True
                 # 窗口左边界收缩
-                cout = s[left]
+                cout = t[left]
                 left += 1
 
                 # 判断当前左边移除的字符是否是需要的字符
@@ -65,4 +62,14 @@ class Solution:
                         valid -= 1
                     window[cout] -= 1
 
-        return '' if Len >= len(s) else s[start: start + Len]
+        return False
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            s = input()
+            t = input()
+            print(Solution().checkIncclusion(s, t))
+        except:
+            break
